7baa125df78bdc888ebf1fa5d70479f157d23181
user <v.baydetska@gmail.com>
2026-02-10 21:30:44 +0200

    lab 14 task 2

--- lab_14/script.js
+++ lab_14/script.js
@@ -1,4 +1,5 @@
 "use strict"
+// task1
 let cities = ["Ternopil", "Lviv", "Warsaw"];
 const citiesDisplay = document.getElementById("cities");
 let citiesString = "";
@@ -8,3 +9,21 @@ for (city of cities){
 }
 citiesString = citiesString.slice(0, -1);
 citiesDisplay.textContent = " " + citiesString;
+
+//task2
+const multDisplay = document.getElementById("multiply");
+let arr = [2, 3, 4, 5];
+let m = 1;
+//через for
+for (let i of arr){
+	m *=i; 	
+	console.log(m);
+}
+multDisplay.textContent += m;
+//через while
+let i = 0;
+while (i < arr.lenght - 1){
+	m *= arr[i]
+	i++
+}
+
