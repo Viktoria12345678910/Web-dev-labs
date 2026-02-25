ac38d2ccfd31dfa3ed1f20ba005e4050f99cfcd7
Viktoria12345678910 <v.baydetska@gmail.com>
2026-02-25 19:54:20 +0200

    lab16

--- lab_16/task3.js
+++ lab_16/task3.js
@@ -1,6 +1,7 @@
 const toggleBtn = document.getElementById('toggleBtn');
         const elements = document.querySelectorAll('.element');
 
+
         toggleBtn.addEventListener('click', function() {
             elements.forEach(element => {
                 element.classList.toggle('hidden');
