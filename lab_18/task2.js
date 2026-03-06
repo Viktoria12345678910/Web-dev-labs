d8f88e7efea832d5281d979b529cfc5639abe02b
Viktoria12345678910 <v.baydetska@gmail.com>
2026-03-06 20:54:34 +0200

    done

---
+++ lab_18/task2.js
@@ -0,0 +1,19 @@
+"use strict"
+function Student(name, age, grade){
+	this.name = name;
+	this.age = age;
+	this.grade = grade;
+	this.study = function(){
+		console.log("|=== "+name+" === "+age+" === "+grade+" ===|");
+	}
+}
+
+let student1 = new Student("Villiam", "19", "5");
+let student2 = new Student("Jd", "17","3");
+let student3 = new Student("alexander", "14", "5+");
+
+console.log("         Students    ");
+console.log("|=== name === age === grade ===|")
+student1.study();
+student2.study();
+student3.study();
