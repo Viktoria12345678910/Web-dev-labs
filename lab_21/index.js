34990ede31561a8b37dee9dd97d351e6c89e190c
Viktoria12345678910 <v.baydetska@gmail.com>
2026-04-03 14:13:33 +0300

    lab 21 fully done

--- lab_21/index.js
+++ lab_21/index.js
@@ -1,73 +1,77 @@
 "use strict";
 const cards = document.getElementById("cards");
 let editingKey = null;
+let n=0;
+let mode = "edit";
 
 const createCard = () => {
-	cards.innerHtml = ""
+	cards.innerHTML = ""
 	const myArray = Object.keys(localStorage).map(key => JSON.parse(localStorage.getItem(key)));
 	for(let product of myArray){	
-	const bottomRow = document.createElement("div");
-	bottomRow.classList.add("bottomRow");
+		const bottomRow = document.createElement("div");
+		bottomRow.classList.add("bottomRow");
 
-	const price = document.createElement("div");
-	price.classList.add("price");
-	price.textContent= product.price;
+		const price = document.createElement("div");
+		price.classList.add("price");
+		price.textContent= product.price;
 
-	const editBtn = document.createElement("i");
-	editBtn.classList.add("fa-regular");
-	editBtn.classList.add("fa-pen-to-square");
-
-	const deleteBtn = document.createElement("i");
-	deleteBtn.classList.add("fa-regular");
-	deleteBtn.classList.add("fa-trash-can");
+		const editBtn = document.createElement("i");
+		editBtn.classList.add("fa-regular");
+		editBtn.classList.add("fa-pen-to-square");
+		editBtn.setAttribute("data-id", product.id);
 
 
-	bottomRow.appendChild(price);
-	bottomRow.appendChild(editBtn);
-	bottomRow.appendChild(deleteBtn);
+		const deleteBtn = document.createElement("i");
+		deleteBtn.classList.add("fa-regular");
+		deleteBtn.classList.add("fa-trash-can");
+		deleteBtn.setAttribute("data-id", product.id);
 
-	const info = document.createElement("div");
-	info.classList.add("info");
+		bottomRow.appendChild(price);
+		bottomRow.appendChild(editBtn);
+		bottomRow.appendChild(deleteBtn);
 
-	const productName = document.createElement("div");
-	productName.classList.add("productName");
-	productName.textContent = product.title;
+		const info = document.createElement("div");
+		info.classList.add("info");
 
-	const productDesc = document.createElement("div");
-	productDesc.classList.add("productDesc");
-	productDesc.textContent = product.description;
+		const productName = document.createElement("div");
+		productName.classList.add("productName");
+		productName.textContent = product.title;
 
-	info.appendChild(productDesc);
-	info.appendChild(productName);
-	info.appendChild(bottomRow);
+		const productDesc = document.createElement("div");
+		productDesc.classList.add("productDesc");
+		productDesc.textContent = product.description;
 
-	const topPart = document.createElement("div");
+		info.appendChild(productDesc);
+		info.appendChild(productName);
+		info.appendChild(bottomRow);
 
-	const badge = document.createElement("div");
-	badge.classList.add("badge");
-	badge.textContent = product.discountPercentage;
+		const topPart = document.createElement("div");
 
-	const photo = document.createElement("img");
-	photo.classList.add("imageArea");
-	photo.setAttribute('src', product.image);
+		const badge = document.createElement("div");
+		badge.classList.add("badge");
+		badge.textContent = product.discountPercentage;
 
-	const cat = document.createElement("div");
-	cat.classList.add("categoryChip");
-	cat.textContent = product.category;
+		const photo = document.createElement("img");
+		photo.classList.add("imageArea");
+		photo.setAttribute('src', product.image);
 
-	topPart.appendChild(badge);
-	topPart.appendChild(photo);
-	topPart.appendChild(cat);
+		const cat = document.createElement("div");
+		cat.classList.add("categoryChip");
+		cat.textContent = product.category;
 
-	const card = document.createElement("div");
-	card.classList.add("card");
-	card.appendChild(topPart);
-	card.appendChild(info);
-	card.setAttribute("data-id", product.id);
-	card.setAttribute("data-rating", product.rating);
-	card.setAttribute("data-stock", product.stock);
-	card.setAttribute("data-brand", product.brand);
-	cards.appendChild(card);
+		topPart.appendChild(badge);
+		topPart.appendChild(photo);
+		topPart.appendChild(cat);
+
+		const card = document.createElement("div");
+		card.classList.add("card");
+		card.appendChild(topPart);
+		card.appendChild(info);
+		card.setAttribute("data-id", product.id);
+		card.setAttribute("data-rating", product.rating);
+		card.setAttribute("data-stock", product.stock);
+		card.setAttribute("data-brand", product.brand);
+		cards.appendChild(card);
 	}
 } 
 createCard();
@@ -78,11 +82,12 @@ createCard();
   }
 
 function openEditModal(key){
-	const product = JSON.parse(localStorege.getIteam(key));
+	console.log(key);
+	const product = JSON.parse(localStorage.getItem(key));
 	editingKey = key;
-
+	console.log(product);
 	document.querySelector("#modal h2").textContent = "Edit product";
-	document.getElentById("title").value = product.title || "";
+	document.getElementById("title").value = product.title || "";
 	document.getElementById("description").value = product.description || "";
 	document.getElementById("price").value = product.price || "";
 	document.getElementById("discount").value = product.discount || "";
@@ -91,8 +96,32 @@ function openEditModal(key){
 	document.getElementById("category").value = product.category || "";
 	document.getElementById("image").value = product.image || "";
 	document.getElementById("overlay").classList.add("active");
+
 }
 
+function openDeleteModal(key){
+	console.log(key);
+	const product = JSON.parse(localStorage.getItem(key));
+	editingKey = key;
+	mode = "delete";
+	console.log(product);
+	document.querySelector("#modal h2").textContent = "Product deletion";
+	document.querySelector("#modal h2").classList.add("deletion");
+	document.getElementById("confirm").textContent = "please confirm that you want to delete product "+product.title;
+	document.querySelectorAll("label").forEach((label) => label.style.display = "none");
+
+	document.getElementById("title").style.display= "none";
+	document.getElementById("description").style.display= "none";
+	document.getElementById("price").style.display= "none";
+	document.getElementById("discount").style.display= "none";
+	document.getElementById("stock").style.display= "none";
+	document.getElementById("brand").style.display= "none";
+	document.getElementById("category").style.display= "none";
+	document.getElementById("image").style.display= "none";
+	document.getElementById("save-btn").textContent = "Delete Product";	
+
+	document.getElementById("overlay").classList.add("active");
+}
   function closeModal() {
     document.getElementById("overlay").classList.remove("active");
     clearForm();
@@ -107,12 +136,24 @@ function openEditModal(key){
       document.getElementById(id).value = "";
       document.getElementById(id).classList.remove("error");
     });
+	document.querySelectorAll("label").forEach((label) => label.style.display = "inline");
+
+	document.getElementById("title").style.display= "inline";
+	document.getElementById("description").style.display= "inline";
+	document.getElementById("price").style.display= "inline";
+	document.getElementById("discount").style.display= "inline";
+	document.getElementById("stock").style.display= "inline";
+	document.getElementById("brand").style.display= "inline";
+	document.getElementById("category").style.display= "inline";
+	document.getElementById("image").style.display= "inline";
+	document.getElementById("save-btn").textContent = "Save Product";	
     document.getElementById("category").value = "";
     document.getElementById("category").classList.remove("error");
     document.querySelectorAll(".error-msg").forEach(el => el.textContent = "");
   }
 
   function validate() {
+	if(mode=="delete") return true;
     let valid = true;
 
     const required = ["title", "description", "price", "discount", "category", "image"];
@@ -141,16 +182,22 @@ function openEditModal(key){
 
     return valid;
   }
-let n = 0;
 const writeToStorage = (obj) => {
-	n +=1;
-	localStorage.setItem("product"+n, JSON.stringify(obj));
+	let key = "product"+n;
+	obj.id=key;
+	localStorage.setItem(key, JSON.stringify(obj));
+	console.log("saved as: ", key);
+	console.log("verify: ", localStorage.getItem(key));
+	n++;
 }
   function saveProduct(e) {
-    e.preventDefault();
-    if (!validate()) return;
+	e.preventDefault();
+	console.log("saveProduct called");
+	console.log("valid: ", validate());
+    	if (!validate()) return;
 
-    const product = {
+    const product = { 
+      id: n, 
       title: document.getElementById("title").value.trim(),
       description: document.getElementById("description").value.trim(),
       price: Number(document.getElementById("price").value),
@@ -160,15 +207,28 @@ const writeToStorage = (obj) => {
       category: document.getElementById("category").value,
       image: document.getElementById("image").value.trim(),
     };
-	if(editingKey){
+	  if (mode == "delete"){
+	  localStorage.removeItem(editingKey);
+	  }
+	  else{
+		if(editingKey){
 		localStorage.setItem(editingKey, JSON.stringify(product));
 	} else {
+		console.log(product);
 		writeToStorage(product);
-	}
+	}}
 
     closeModal();
     createCard();
   }
-
-function edit(e){
-	if(e.target.classlist.conatins("fa-pen-to-square")){})}
+cards.addEventListener("click", editOrDelete);
+function editOrDelete(e){
+	let key = e.target.dataset.id;
+	if(e.target.classList.contains("fa-pen-to-square")){ 
+		openEditModal("product"+key);
+	} else if(e.target.classList.contains("fa-trash-can")){
+		openDeleteModal("product"+key);
+		createCard();
+		console.log("delete called");
+	}
+}
